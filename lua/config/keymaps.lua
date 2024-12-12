3b4c1603727cfe948aa51be2648771b5c66d4384
Colin Gemmell <pythonandchips@gmail.com>
2024-12-12 21:32:54 +0000

    reset my quick navs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
+map("", "<leader>e", ":Explore . <CR>", default_opts)
+map("", "<leader>E", ":Explore <CR>", default_opts)
