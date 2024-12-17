c7cc72f541105499349f3259ca19e4c63ff6c510
Colin Gemmell <pythonandchips@gmail.com>
2024-12-17 19:15:53 +0000

    add status line everywhere and update the leader key

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,10 @@
 -- Add any additional options here
 --
 vim.opt.relativenumber = false
+vim.g.mapleader = ","
 
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
+
+vim.o.laststatus = 2
+
